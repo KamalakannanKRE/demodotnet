ef2dfb65aa51fc4ff527e5c5ffde5673cec9e05d
Kamalakannan K <kamalakannan.k@ctlep.local>
2024-01-31 10:56:43 +0530

    demo dotnet app

--- obj/Debug/net7.0/DotNet.AssemblyInfo.cs
+++ obj/Debug/net7.0/DotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5644d81d5a145d5a18004408fe072a0b51c35c63")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
